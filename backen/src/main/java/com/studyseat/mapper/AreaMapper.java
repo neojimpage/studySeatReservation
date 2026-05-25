969ca36626ad5abd67c6417bc622d050e0592c79
jianai <1265605424@qq.com>
2026-05-25 14:58:12 +0800

    又对项目进行了一些完善

--- backen/src/main/java/com/studyseat/mapper/AreaMapper.java
+++
@@ -1,7 +0,0 @@
-package com.studyseat.mapper;
-
-import com.studyseat.entity.Area;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface AreaMapper extends BaseMapper<Area> {
-}
--- backen/src/main/java/com/studyseat/service/AreaService.java
+++
@@ -1,9 +0,0 @@
-package com.studyseat.service;
-
-import com.studyseat.entity.Area;
-import java.util.List;
-
-public interface AreaService {
-    List<Area> getAllAreas();
-    Area getById(Long id);
-}
